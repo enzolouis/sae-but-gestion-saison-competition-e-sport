a6df004345c22ea5ea8139fb9b98084146de182d
Olivier-tlse3 <olivier.rodriguez@etu.iut-tlse3.fr>
2023-11-20 08:16:05 +0100

    Create TEST_Arbitre.java

---
+++ sae_esport/src/DAOs/TEST_Arbitre.java
@@ -0,0 +1,14 @@
+package DAOs;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TEST_Arbitre {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
