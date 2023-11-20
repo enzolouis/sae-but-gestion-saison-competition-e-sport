2fb8afdcfe5b18299006cdec73f4d6b17f38843a
Pema <rasoanaivo.pema@gmail.com>
2023-11-20 08:17:34 +0100

    Enumeration Nationalite ajoutée

---
+++ sae_esport/src/Nationalite.java
@@ -0,0 +1,401 @@
+public enum Nationalite {
+    // Afghanistan:
+    AF("Afghanistan"),
+    // Albania:
+    AL("Albania"),
+    // Algeria:
+    DZ("Algeria"),
+    // Andorra:
+    AD("Andorra"),
+    // Angola:
+    AO("Angola"),
+    // Antigua and Barbuda:
+    AG("Antigua and Barbuda"),
+    // Argentina:
+    AR("Argentina"),
+    // Armenia:
+    AM("Armenia"),
+    // Australia:
+    AU("Australia"),
+    // Austria:
+    AT("Austria"),
+    // Azerbaijan:
+    AZ("Azerbaijan"),
+    // Bahamas:
+    BS("Bahamas"),
+    // Bahrain:
+    BH("Bahrain"),
+    // Bangladesh:
+    BD("Bangladesh"),
+    // Barbados:
+    BB("Barbados"),
+    // Belarus:
+    BY("Belarus"),
+    // Belgium:
+    BE("Belgium"),
+    // Belize:
+    BZ("Belize"),
+    // Benin:
+    BJ("Benin"),
+    // Bhutan:
+    BT("Bhutan"),
+    // Bolivia:
+    BO("Bolivia"),
+    // Bosnia and Herzegovina:
+    BA("Bosnia and Herzegovina"),
+    // Botswana:
+    BW("Botswana"),
+    // Brazil:
+    BR("Brazil"),
+    // Brunei:
+    BN("Brunei"),
+    // Bulgaria:
+    BG("Bulgaria"),
+    // Burkina Faso:
+    BF("Burkina Faso"),
+    // Burundi:
+    BI("Burundi"),
+    // Cabo Verde:
+    CV("Cabo Verde"),
+    // Cambodia:
+    KH("Cambodia"),
+    // Cameroon:
+    CM("Cameroon"),
+    // Canada:
+    CA("Canada"),
+    // Central African Republic:
+    CF("Central African Republic"),
+    // Chad:
+    TD("Chad"),
+    // Chile:
+    CL("Chile"),
+    // China:
+    CN("China"),
+    // Colombia:
+    CO("Colombia"),
+    // Comoros:
+    KM("Comoros"),
+    // Congo (Congo-Brazzaville):
+    CG("Congo (Congo-Brazzaville)"),
+    // Costa Rica:
+    CR("Costa Rica"),
+    // Croatia:
+    HR("Croatia"),
+    // Cuba:
+    CU("Cuba"),
+    // Cyprus:
+    CY("Cyprus"),
+    // Czechia (Czech Republic):
+    CZ("Czechia (Czech Republic)"),
+    // Denmark:
+    DK("Denmark"),
+    // Djibouti:
+    DJ("Djibouti"),
+    // Dominica:
+    DM("Dominica"),
+    // Dominican Republic:
+    DO("Dominican Republic"),
+    // Ecuador:
+    EC("Ecuador"),
+    // Egypt:
+    EG("Egypt"),
+    // El Salvador:
+    SV("El Salvador"),
+    // Equatorial Guinea:
+    GQ("Equatorial Guinea"),
+    // Eritrea:
+    ER("Eritrea"),
+    // Estonia:
+    EE("Estonia"),
+    // Eswatini:
+    SZ("Eswatini"),
+    // Ethiopia:
+    ET("Ethiopia"),
+    // Fiji:
+    FJ("Fiji"),
+    // Finland:
+    FI("Finland"),
+    // France:
+    FR("France"),
+    // Gabon:
+    GA("Gabon"),
+    // Gambia:
+    GM("Gambia"),
+    // Georgia:
+    GE("Georgia"),
+    // Germany:
+    DE("Germany"),
+    // Ghana:
+    GH("Ghana"),
+    // Greece:
+    GR("Greece"),
+    // Grenada:
+    GD("Grenada"),
+    // Guatemala:
+    GT("Guatemala"),
+    // Guinea:
+    GN("Guinea"),
+    // Guinea-Bissau:
+    GW("Guinea-Bissau"),
+    // Guyana:
+    GY("Guyana"),
+    // Haiti:
+    HT("Haiti"),
+    // Honduras:
+    HN("Honduras"),
+    // Hungary:
+    HU("Hungary"),
+    // Iceland:
+    IS("Iceland"),
+    // India:
+    IN("India"),
+    // Indonesia:
+    ID("Indonesia"),
+    // Iran:
+    IR("Iran"),
+    // Iraq:
+    IQ("Iraq"),
+    // Ireland:
+    IE("Ireland"),
+    // Israel:
+    IL("Israel"),
+    // Italy:
+    IT("Italy"),
+    // Jamaica:
+    JM("Jamaica"),
+    // Japan:
+    JP("Japan"),
+    // Jordan:
+    JO("Jordan"),
+    // Kazakhstan:
+    KZ("Kazakhstan"),
+    // Kenya:
+    KE("Kenya"),
+    // Kiribati:
+    KI("Kiribati"),
+    // Korea, North:
+    KP("Korea, North"),
+    // Korea, South:
+    KR("Korea, South"),
+    // Kosovo:
+    XK("Kosovo"),
+    // Kuwait:
+    KW("Kuwait"),
+    // Kyrgyzstan:
+    KG("Kyrgyzstan"),
+    // Laos:
+    LA("Laos"),
+    // Latvia:
+    LV("Latvia"),
+    // Lebanon:
+    LB("Lebanon"),
+    // Lesotho:
+    LS("Lesotho"),
+    // Liberia:
+    LR("Liberia"),
+    // Libya:
+    LY("Libya"),
+    // Liechtenstein:
+    LI("Liechtenstein"),
+    // Lithuania:
+    LT("Lithuania"),
+    // Luxembourg:
+    LU("Luxembourg"),
+    // Madagascar:
+    MG("Madagascar"),
+    // Malawi:
+    MW("Malawi"),
+    // Malaysia:
+    MY("Malaysia"),
+    // Maldives:
+    MV("Maldives"),
+    // Mali:
+    ML("Mali"),
+    // Malta:
+    MT("Malta"),
+    // Marshall Islands:
+    MH("Marshall Islands"),
+    // Mauritania:
+    MR("Mauritania"),
+    // Mauritius:
+    MU("Mauritius"),
+    // Mexico:
+    MX("Mexico"),
+    // Micronesia:
+    FM("Micronesia"),
+    // Moldova:
+    MD("Moldova"),
+    // Monaco:
+    MC("Monaco"),
+    // Mongolia:
+    MN("Mongolia"),
+    // Montenegro:
+    ME("Montenegro"),
+    // Morocco:
+    MA("Morocco"),
+    // Mozambique:
+    MZ("Mozambique"),
+    // Myanmar (Burma):
+    MM("Myanmar (Burma)"),
+    // Namibia:
+    NA("Namibia"),
+    // Nauru:
+    NR("Nauru"),
+    // Nepal:
+    NP("Nepal"),
+    // Netherlands:
+    NL("Netherlands"),
+    // New Zealand:
+    NZ("New Zealand"),
+    // Nicaragua:
+    NI("Nicaragua"),
+    // Niger:
+    NE("Niger"),
+    // Nigeria:
+    NG("Nigeria"),
+    // North Macedonia (formerly Macedonia):
+    MK("North Macedonia (formerly Macedonia)"),
+    // Norway:
+    NO("Norway"),
+    // Oman:
+    OM("Oman"),
+    // Pakistan:
+    PK("Pakistan"),
+    // Palau:
+    PW("Palau"),
+    // Panama:
+    PA("Panama"),
+    // Papua New Guinea:
+    PG("Papua New Guinea"),
+    // Paraguay:
+    PY("Paraguay"),
+    // Peru:
+    PE("Peru"),
+    // Philippines:
+    PH("Philippines"),
+    // Poland:
+    PL("Poland"),
+    // Portugal:
+    PT("Portugal"),
+    // Qatar:
+    QA("Qatar"),
+    // Romania:
+    RO("Romania"),
+    // Russia:
+    RU("Russia"),
+    // Rwanda:
+    RW("Rwanda"),
+    // Saint Kitts and Nevis:
+    KN("Saint Kitts and Nevis"),
+    // Saint Lucia:
+    LC("Saint Lucia"),
+    // Saint Vincent and the Grenadines:
+    VC("Saint Vincent and the Grenadines"),
+    // Samoa:
+    WS("Samoa"),
+    // San Marino:
+    SM("San Marino"),
+    // Sao Tome and Principe:
+    ST("Sao Tome and Principe"),
+    // Saudi Arabia:
+    SA("Saudi Arabia"),
+    // Senegal:
+    SN("Senegal"),
+    // Serbia:
+    RS("Serbia"),
+    // Seychelles:
+    SC("Seychelles"),
+    // Sierra Leone:
+    SL("Sierra Leone"),
+    // Singapore:
+    SG("Singapore"),
+    // Slovakia:
+    SK("Slovakia"),
+    // Slovenia:
+    SI("Slovenia"),
+    // Solomon Islands:
+    SB("Solomon Islands"),
+    // Somalia:
+    SO("Somalia"),
+    // South Africa:
+    ZA("South Africa"),
+    // South Sudan:
+    SS("South Sudan"),
+    // Spain:
+    ES("Spain"),
+    // Sri Lanka:
+    LK("Sri Lanka"),
+    // Sudan:
+    SD("Sudan"),
+    // Suriname:
+    SR("Suriname"),
+    // Sweden:
+    SE("Sweden"),
+    // Switzerland:
+    CH("Switzerland"),
+    // Syria:
+    SY("Syria"),
+    // Taiwan:
+    TW("Taiwan"),
+    // Tajikistan:
+    TJ("Tajikistan"),
+    // Tanzania:
+    TZ("Tanzania"),
+    // Thailand:
+    TH("Thailand"),
+    // Timor-Leste:
+    TL("Timor-Leste"),
+    // Togo:
+    TG("Togo"),
+    // Tonga:
+    TO("Tonga"),
+    // Trinidad and Tobago:
+    TT("Trinidad and Tobago"),
+    // Tunisia:
+    TN("Tunisia"),
+    // Turkey:
+    TR("Turkey"),
+    // Turkmenistan:
+    TM("Turkmenistan"),
+    // Tuvalu:
+    TV("Tuvalu"),
+    // Uganda:
+    UG("Uganda"),
+    // Ukraine:
+    UA("Ukraine"),
+    // United Arab Emirates:
+    AE("United Arab Emirates"),
+    // United Kingdom:
+    GB("United Kingdom"),
+    // United States:
+    US("United States"),
+    // Uruguay:
+    UY("Uruguay"),
+    // Uzbekistan:
+    UZ("Uzbekistan"),
+    // Vanuatu:
+    VU("Vanuatu"),
+    // Vatican City:
+    VA("Vatican City"),
+    // Venezuela:
+    VE("Venezuela"),
+    // Vietnam:
+    VN("Vietnam"),
+    // Yemen:
+    YE("Yemen"),
+    // Zambia:
+    ZM("Zambia"),
+    // Zimbabwe:
+    ZW("Zimbabwe");
+    
+    private String country;
+
+    Nationalite(String country) {
+        this.country = country;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+    
+}
