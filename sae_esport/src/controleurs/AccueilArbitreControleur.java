a7c1c6819dd6631fac358604e0e2f1e0a3cf344e
Ember <65914622+EmberOn@users.noreply.github.com>
2023-11-24 16:50:30 +0100

    Implémentation du contrôleur des accueils

--- sae_esport/src/controleurs/AccueilArbitreControleur.java
+++ sae_esport/src/controleurs/AccueilArbitreControleur.java
@@ -3,6 +3,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
 
+import javax.swing.JButton;
+
 import modeles.AccueilArbitreModele;
 import vues.AccueilArbitreVue;
 
@@ -20,7 +22,11 @@ public class AccueilArbitreControleur implements ActionListener {
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		System.out.println("controleur page administrateur");
+		JButton bouton = (JButton) e.getSource();
+		if (bouton.getText().equals("Quitter")) {
+			this.vue.setVisible(false);
+			this.vue.dispose();
+		}
 	}
 
 }
