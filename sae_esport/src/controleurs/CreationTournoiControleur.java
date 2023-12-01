459c1504254e27890819bb686f82bb443a363560
Pema <rasoanaivo.pema@gmail.com>
2023-12-01 16:46:02 +0100

    Ebauche CreationTournoiControleur

--- sae_esport/src/controleurs/CreationTournoiControleur.java
+++ sae_esport/src/controleurs/CreationTournoiControleur.java
@@ -4,6 +4,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
 
+import javax.swing.JButton;
+
+import classes.Arbitre;
 import modeles.CreationTournoiModele;
 import vues.CreationTournoiVue;
 
@@ -20,7 +23,36 @@ public class CreationTournoiControleur implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
+		if (e.getSource() instanceof JButton) {
+			JButton bouton = (JButton) e.getSource();
+			switch (bouton.getText()) {
+			case ("Ajouter"):
+				this.vue.modeleList.addElement((Arbitre) this.vue.comboBoxArbitre.getSelectedItem());
+				break;
+			case ("Vider"):
+				this.vue.modeleList.removeAllElements();
+				break;
+			case ("Supprimer"):
+				for (Arbitre a : this.vue.listArbitres.getSelectedValuesList()) {
+					this.vue.modeleList.removeElement(a);
+				}
+				break;
+			case ("Importer"):
+				if (!this.vue.textFieldEquipesFile.getText().isEmpty()) {
+					//code d'ajout de l'importation des équipes
+				}
+				break;
+			case ("Quitter"):
+				break;
+			case ("Valider"):
+				break;
+			}
+		}
+	}
+	
+	public boolean checkAllFields() {
+		return this.vue.textFieldEquipesFile.getText().isEmpty() ||
+				this.vue.textFieldNom.getText().isEmpty();
 	}
 	
 }
--- sae_esport/src/vues/CreationTournoiVue.java
+++ sae_esport/src/vues/CreationTournoiVue.java
@@ -29,13 +29,26 @@ import java.awt.EventQueue;
 
 import javax.swing.JButton;
 import javax.swing.BoxLayout;
+import javax.swing.DefaultListModel;
 import javax.swing.JFileChooser;
 import javax.swing.JList;
 
 public class CreationTournoiVue extends JFrame {
 	
 	JPanel contentPane;
-	private JTextField textFieldEquipesFile;
+	public JTextField textFieldEquipesFile;
+	public JTextField textFieldNom;
+	public JComboBox<Notoriete> comboBoxNotoriete;
+	public JDateChooser dateChooserDebut;
+	public JDateChooser dateChooserFin;
+	public JComboBox<Arbitre> comboBoxArbitre;
+	public JButton btnAddArbitre;
+	public JButton btnValider;
+	public JButton btnImportEquipes;
+	public JButton btnSupprimerArbitre;
+	public JButton btnViderArbitres;
+	public JList<Arbitre> listArbitres;
+	public DefaultListModel<Arbitre> modeleList;
 	
 	public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
@@ -57,60 +70,68 @@ public class CreationTournoiVue extends JFrame {
     }
 	
 	public CreationTournoiVue(Connection dbConnection) {
-		setMinimumSize(new Dimension(858, 520));
 		
+		setMinimumSize(new Dimension(858, 520));
+		Color backgroundColor = new Color(44, 47, 51);
+		Color policeColor = new Color(107,173,221);
+		
+		//création du panel principal
 		contentPane = new JPanel();
 		setContentPane(contentPane);
-		contentPane.setBackground(new Color(44, 47, 51));
+		contentPane.setBackground(backgroundColor);
 		contentPane.setLayout(new BorderLayout());
 		contentPane.setBorder(new EmptyBorder(30,30,30,30));
+		
+		//création du panel contenant le titre
 		JPanel panelTitre = new JPanel();
 		panelTitre.setBorder(new EmptyBorder(10, 10, 10, 10));
-		panelTitre.setBackground(new Color(44, 47, 51));
+		panelTitre.setBackground(backgroundColor);
+		
 		contentPane.add(panelTitre, BorderLayout.NORTH);
 		panelTitre.setLayout(new BorderLayout(0, 0));
-		
 		JSeparator separatorTitre = new JSeparator();
-		separatorTitre.setForeground(new Color(102, 173, 221));
-		separatorTitre.setBackground(new Color(102, 173, 221));
+		separatorTitre.setForeground(policeColor);
+		separatorTitre.setBackground(policeColor);
 		panelTitre.add(separatorTitre, BorderLayout.SOUTH);
 		
 		JLabel lblCreationDeTournoi = new JLabel("CREATION DE TOURNOI");
 		lblCreationDeTournoi.setHorizontalAlignment(SwingConstants.CENTER);
-		lblCreationDeTournoi.setForeground(new Color(102, 173, 221));
+		lblCreationDeTournoi.setForeground(policeColor);
 		lblCreationDeTournoi.setFont(new Font("Tahoma", Font.BOLD, 25));
 		lblCreationDeTournoi.setBorder(new EmptyBorder(10, 10, 10, 10));
 		panelTitre.add(lblCreationDeTournoi, BorderLayout.CENTER);
 		
+		//création du panel de contenu
 		JPanel panelContenu = new JPanel();
 		panelContenu.setBorder(new EmptyBorder(5, 5, 5, 5));
-		panelContenu.setBackground(new Color(44, 47, 51));
+		panelContenu.setBackground(backgroundColor);
 		contentPane.add(panelContenu, BorderLayout.CENTER);
 		panelContenu.setLayout(new GridLayout(0, 2, 0, 0));
 		
 		JPanel panelInformations = new JPanel();
-		panelInformations.setBackground(new Color(44, 47, 51));
+		panelInformations.setBackground(backgroundColor);
 		panelContenu.add(panelInformations);
 		panelInformations.setLayout(new GridLayout(2, 0, 0, 0));
 		
+		//création du panel contenant la saisie du nom et de la notoriété
 		JPanel panelNomNotoriete = new JPanel();
-		panelNomNotoriete.setBackground(new Color(44, 47, 51));
+		panelNomNotoriete.setBackground(backgroundColor);
 		panelInformations.add(panelNomNotoriete);
 		panelNomNotoriete.setLayout(new GridLayout(2, 0, 0, 0));
 		
 		JPanel panelNom = new JPanel();
 		panelNom.setBorder(new EmptyBorder(5, 5, 5, 5));
-		panelNom.setBackground(new Color(44, 47, 51));
+		panelNom.setBackground(backgroundColor);
 		panelNomNotoriete.add(panelNom);
 		panelNom.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
 		JLabel lbNom = new JLabel("Nom du tournoi :");
 		lbNom.setPreferredSize(new Dimension(110, 30));
 		lbNom.setHorizontalAlignment(SwingConstants.RIGHT);
-		lbNom.setForeground(new Color(107, 173, 221));
+		lbNom.setForeground(policeColor);
 		panelNom.add(lbNom);
 		
-		JTextField textFieldNom = new JTextField();
+		this.textFieldNom = new JTextField();
 		textFieldNom.setPreferredSize(new Dimension(110, 30));
 		textFieldNom.setHorizontalAlignment(SwingConstants.CENTER);
 		panelNom.add(textFieldNom);
@@ -118,142 +139,144 @@ public class CreationTournoiVue extends JFrame {
 		
 		JPanel panelNotoriete = new JPanel();
 		panelNotoriete.setBorder(new EmptyBorder(5, 5, 5, 5));
-		panelNotoriete.setBackground(new Color(44, 47, 51));
+		panelNotoriete.setBackground(backgroundColor);
 		panelNomNotoriete.add(panelNotoriete);
 		
 		JLabel lbNotoriete = new JLabel("Notoriété :");
 		lbNotoriete.setHorizontalAlignment(SwingConstants.RIGHT);
 		lbNotoriete.setPreferredSize(new Dimension(110, 30));
-		lbNotoriete.setForeground(new Color(107, 173, 221));
+		lbNotoriete.setForeground(policeColor);
 		panelNotoriete.add(lbNotoriete);
 		
-		JComboBox<Notoriete> comboBoxNotoriete = new JComboBox<Notoriete>();
-		comboBoxNotoriete.setPreferredSize(new Dimension(95, 30));
+		this.comboBoxNotoriete = new JComboBox<Notoriete>();
+		this.comboBoxNotoriete.setPreferredSize(new Dimension(95, 30));
 		panelNotoriete.add(comboBoxNotoriete);
 		for (Notoriete n : Notoriete.values()) {
-			comboBoxNotoriete.addItem(n);
+			this.comboBoxNotoriete.addItem(n);
 		}
 		
+		//création du panel
 		JPanel panelDates = new JPanel();
-		panelDates.setBackground(new Color(44, 47, 51));
+		panelDates.setBackground(backgroundColor);
 		panelInformations.add(panelDates);
 		panelDates.setLayout(new GridLayout(2, 0, 0, 0));
 		
 		JPanel panelDateDebut = new JPanel();
 		panelDateDebut.setBorder(new EmptyBorder(5, 5, 5, 5));
-		panelDateDebut.setBackground(new Color(44, 47, 51));
+		panelDateDebut.setBackground(backgroundColor);
 		panelDates.add(panelDateDebut);
 		
 		JLabel lblDateDeDbut = new JLabel("Date de début :");
 		lblDateDeDbut.setPreferredSize(new Dimension(100, 30));
 		lblDateDeDbut.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblDateDeDbut.setForeground(new Color(107, 173, 221));
+		lblDateDeDbut.setForeground(policeColor);
 		panelDateDebut.add(lblDateDeDbut);
 		
-		JDateChooser dateChooser = new JDateChooser();
-		dateChooser.setPreferredSize(new Dimension(100, 30));
-		panelDateDebut.add(dateChooser);
+		this.dateChooserDebut = new JDateChooser();
+		this.dateChooserDebut.setPreferredSize(new Dimension(100, 30));
+		panelDateDebut.add(this.dateChooserDebut);
 		
 		JPanel panelDateFin = new JPanel();
 		panelDateFin.setBorder(new EmptyBorder(5, 5, 5, 5));
-		panelDateFin.setBackground(new Color(44, 47, 51));
+		panelDateFin.setBackground(backgroundColor);
 		panelDates.add(panelDateFin);
 		
 		JLabel lblDateDeFin = new JLabel("Date de fin :");
 		lblDateDeFin.setPreferredSize(new Dimension(100, 30));
 		lblDateDeFin.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblDateDeFin.setForeground(new Color(107, 173, 221));
+		lblDateDeFin.setForeground(policeColor);
 		panelDateFin.add(lblDateDeFin);
 		
-		JDateChooser dateChooser_1 = new JDateChooser();
-		dateChooser_1.setPreferredSize(new Dimension(100, 30));
-		panelDateFin.add(dateChooser_1);
+		this.dateChooserFin = new JDateChooser();
+		this.dateChooserFin.setPreferredSize(new Dimension(100, 30));
+		panelDateFin.add(this.dateChooserFin);
 		
 		JPanel panelEquipesArbitres = new JPanel();
-		panelEquipesArbitres.setBackground(new Color(44, 47, 51));
+		panelEquipesArbitres.setBackground(backgroundColor);
 		panelContenu.add(panelEquipesArbitres);
 		panelEquipesArbitres.setLayout(new BorderLayout(0, 0));
 		
 		JPanel panelArbitre = new JPanel();
 		panelArbitre.setBorder(new EmptyBorder(10, 10, 10, 10));
-		panelArbitre.setBackground(new Color(44, 47, 51));
+		panelArbitre.setBackground(backgroundColor);
 		panelEquipesArbitres.add(panelArbitre, BorderLayout.CENTER);
 		panelArbitre.setLayout(new BorderLayout(0, 0));
 		
 		JLabel lblChoixArbitres = new JLabel("Séléction des arbitres");
 		lblChoixArbitres.setHorizontalAlignment(SwingConstants.CENTER);
-		lblChoixArbitres.setForeground(new Color(107, 173, 221));
+		lblChoixArbitres.setForeground(policeColor);
 		panelArbitre.add(lblChoixArbitres, BorderLayout.NORTH);
 		
 		JPanel panelChoixArbitre = new JPanel();
-		panelChoixArbitre.setBackground(new Color(44, 47, 51));
+		panelChoixArbitre.setBackground(backgroundColor);
 		panelChoixArbitre.setBorder(new EmptyBorder(10, 10, 10, 10));
 		panelArbitre.add(panelChoixArbitre, BorderLayout.CENTER);
 		
-		JComboBox<Arbitre> comboBoxArbitre = new JComboBox<Arbitre>();
+		this.comboBoxArbitre = new JComboBox<Arbitre>();
 		panelChoixArbitre.add(comboBoxArbitre);
-		comboBoxArbitre.addItem(new Arbitre(0, "arbitre", "test", Nationalite.FR));
-		comboBoxArbitre.addItem(new Arbitre(1, "arbitre", "tentative", Nationalite.FR));
+		this.comboBoxArbitre.addItem(new Arbitre(0, "arbitre", "test", Nationalite.FR));
+		this.comboBoxArbitre.addItem(new Arbitre(1, "arbitre", "tentative", Nationalite.FR));
 		
-		JButton btnAddArbitre = new JButton("Ajouter");
-		panelChoixArbitre.add(btnAddArbitre);
+		this.btnAddArbitre = new JButton("Ajouter");
+		panelChoixArbitre.add(this.btnAddArbitre);
 		
 		JPanel panelListeArbitres = new JPanel();
 		panelListeArbitres.setBorder(new EmptyBorder(10, 10, 10, 10));
-		panelListeArbitres.setBackground(new Color(44, 47, 51));
+		panelListeArbitres.setBackground(backgroundColor);
 		panelArbitre.add(panelListeArbitres, BorderLayout.SOUTH);
 		panelListeArbitres.setLayout(new BorderLayout(0, 0));
 		
-		JList<Arbitre> list = new JList<Arbitre>();
-		list.setMinimumSize(new Dimension(100, 100));
-		panelListeArbitres.add(list);
+		this.listArbitres = new JList<Arbitre>();
+		listArbitres.setMinimumSize(new Dimension(100, 100));
+		this.listArbitres.setModel(modeleList);
+		panelListeArbitres.add(listArbitres);
 		
 		JPanel panelBtnsArbitre = new JPanel();
-		panelBtnsArbitre.setBackground(new Color(44, 47, 51));
+		panelBtnsArbitre.setBackground(backgroundColor);
 		panelListeArbitres.add(panelBtnsArbitre, BorderLayout.SOUTH);
 		
-		JButton btnViderArbitres = new JButton("Vider");
+		this.btnViderArbitres = new JButton("Vider");
 		panelBtnsArbitre.add(btnViderArbitres);
 		
-		JButton btnSupprimerArbitre = new JButton("Supprimer");
-		panelBtnsArbitre.add(btnSupprimerArbitre);
+		this.btnSupprimerArbitre = new JButton("Supprimer");
+		panelBtnsArbitre.add(this.btnSupprimerArbitre);
 		
 		JPanel panelEquipes = new JPanel();
-		panelEquipes.setBackground(new Color(44, 47, 51));
+		panelEquipes.setBackground(backgroundColor);
 		panelEquipesArbitres.add(panelEquipes, BorderLayout.SOUTH);
 		
 		JLabel lblEquipesFile = new JLabel("Fichier des équipes :");
 		lblEquipesFile.setForeground(new Color(107, 173, 221));
 		panelEquipes.add(lblEquipesFile);
 		
-		textFieldEquipesFile = new JTextField();
-		textFieldEquipesFile.setEnabled(false);
-		textFieldEquipesFile.setEditable(false);
-		panelEquipes.add(textFieldEquipesFile);
-		textFieldEquipesFile.setColumns(10);
+		this.textFieldEquipesFile = new JTextField();
+		this.textFieldEquipesFile.setEnabled(false);
+		this.textFieldEquipesFile.setEditable(false);
+		panelEquipes.add(this.textFieldEquipesFile);
+		this.textFieldEquipesFile.setColumns(10);
 		
-		JButton btnImportEquipes = new JButton("Importer");
-		panelEquipes.add(btnImportEquipes);
+		this.btnImportEquipes = new JButton("Importer");
+		panelEquipes.add(this.btnImportEquipes);
 		
 		JPanel panelValidation = new JPanel();
 		panelValidation.setBorder(new EmptyBorder(20, 20, 20, 20));
-		panelValidation.setBackground(new Color(44, 47, 51));
+		panelValidation.setBackground(backgroundColor);
 		contentPane.add(panelValidation, BorderLayout.SOUTH);
 		panelValidation.setLayout(new GridLayout(0, 2, 0, 0));
 		
 		JPanel panel_2 = new JPanel();
-		panel_2.setBackground(new Color(44, 47, 51));
+		panel_2.setBackground(backgroundColor);
 		panelValidation.add(panel_2);
 		
 		JButton btnQuitter = new JButton("Quitter");
 		panel_2.add(btnQuitter);
 		
 		JPanel panel_3 = new JPanel();
-		panel_3.setBackground(new Color(44, 47, 51));
+		panel_3.setBackground(backgroundColor);
 		panelValidation.add(panel_3);
 		
-		JButton btnValider = new JButton("Valider");
-		panel_3.add(btnValider);
+		this.btnValider = new JButton("Valider");
+		panel_3.add(this.btnValider);
 
 	}
 
