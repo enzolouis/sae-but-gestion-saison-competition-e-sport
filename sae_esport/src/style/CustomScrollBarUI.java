c3d476ec654fcf29fa9dfa074c476c5165fbebe6
Ryan <mordeckai.pro@gmail.com>
2023-12-03 19:55:29 +0100

    Create CustomScrollBarUI.java

---
+++ sae_esport/src/style/CustomScrollBarUI.java
@@ -0,0 +1,49 @@
+package style;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.RenderingHints;
+
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.plaf.basic.BasicScrollBarUI;
+
+public class CustomScrollBarUI extends BasicScrollBarUI {
+	
+	@Override     
+    protected void paintThumb(Graphics g, JComponent c, Rectangle thumbBounds) {         
+    	Graphics2D g2 = (Graphics2D) g;         
+    	g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);         
+    	g2.setColor(new Color(29, 88, 129));         
+    	g2.fillRoundRect(thumbBounds.x, thumbBounds.y, thumbBounds.width, thumbBounds.height, 15, 15);     
+    }          
+    
+    @Override     
+    protected void paintTrack(Graphics g, JComponent c, Rectangle trackBounds) {         
+    	Graphics2D g2 = (Graphics2D) g;         
+    	g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);         
+    	g2.setColor(new Color(44, 47, 51));         
+    	g2.fillRoundRect(trackBounds.x, trackBounds.y, trackBounds.width, trackBounds.height, 0, 0);     
+    }
+    
+    @Override
+    protected JButton createDecreaseButton(int orientation) {
+        return createZeroButton();
+    }
+
+    @Override    
+    protected JButton createIncreaseButton(int orientation) {
+        return createZeroButton();
+    }
+
+    private JButton createZeroButton() {
+        JButton jbutton = new JButton();
+        jbutton.setPreferredSize(new Dimension(0, 0));
+        jbutton.setMinimumSize(new Dimension(0, 0));
+        jbutton.setMaximumSize(new Dimension(0, 0));
+        return jbutton;
+    }
+}
