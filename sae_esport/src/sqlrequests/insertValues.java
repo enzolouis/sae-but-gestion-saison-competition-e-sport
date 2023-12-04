39df25e488e5685350f80f774622091d76157375
Olivier-tlse3 <olivier.rodriguez@etu.iut-tlse3.fr>
2023-12-04 10:03:17 +0100

    Update insertValues.java

--- sae_esport/src/sqlrequests/insertValues.java
+++ sae_esport/src/sqlrequests/insertValues.java
@@ -14,20 +14,12 @@ public class insertValues {
 
     public static void main(String[] args) throws Exception {
     	
-    	//connexion à la base de données
-
-        String dirProjetJava = System.getProperty("user.dir");
-        System.setProperty("derby.system.home", dirProjetJava+"/BDDSAEEsport");
-        DriverManager.registerDriver(new org.apache.derby.jdbc.EmbeddedDriver());
-        String urlConnexion = "jdbc:derby:BDDSAEEsport;create=true";
-        Connection dbConnection = DriverManager.getConnection(urlConnexion);
-        
         //ajout d'administrateur
-        AdministrateurDAO adminDAO = new AdministrateurDAO(dbConnection);
+        AdministrateurDAO adminDAO = new AdministrateurDAO();
         adminDAO.add(new Administrateur(0, "admin1", "admin1", "motdepasse"));
         System.out.print("Admin OK");
         
-        TournoiDAO tournoiDAO = new TournoiDAO(dbConnection);
+        TournoiDAO tournoiDAO = new TournoiDAO();
         tournoiDAO.add(new Tournoi(0, "premierTournoi", "2023-12-16", "2023-12-24", Notoriete.LOCAL, EtatTournoi.OUVERT));
         System.out.print("Tournoi OK");
     }
