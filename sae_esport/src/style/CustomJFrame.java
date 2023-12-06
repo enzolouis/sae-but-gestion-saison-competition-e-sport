494093e3fcf6add9a481655bbe44054d4dd27998
Ryan <mordeckai.pro@gmail.com>
2023-12-06 10:32:01 +0100

    CustomJFrame : Dimension fix

--- sae_esport/src/style/CustomJFrame.java
+++ sae_esport/src/style/CustomJFrame.java
@@ -7,13 +7,13 @@ import javax.swing.JFrame;
 public class CustomJFrame extends JFrame {
 	CustomJPanel contentPanel;
 	
-	public CustomJFrame() {
+	public CustomJFrame(int[] dimension) {
 		super();
 		
 		this.contentPanel = new CustomJPanel(new int[]{30, 30, 30, 30});
-    	setMinimumSize(new Dimension(900, 600));
+    	setMinimumSize(new Dimension(dimension[0], dimension[1]));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(100, 100, 450, 300);
+        setBounds(100, 100, dimension[0], dimension[1]);
         setContentPane(this.contentPanel);
 	}
 	
