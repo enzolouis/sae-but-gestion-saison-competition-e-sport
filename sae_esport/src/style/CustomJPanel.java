c9af3e59ce138426403b114291a20be2512f7cae
Ryan <mordeckai.pro@gmail.com>
2023-12-07 16:44:28 +0100

    CustomJPanel : Ajout constructeur

--- sae_esport/src/style/CustomJPanel.java
+++ sae_esport/src/style/CustomJPanel.java
@@ -2,6 +2,9 @@ package style;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -16,11 +19,43 @@ public class CustomJPanel extends JPanel {
     	this.setLayout(new BorderLayout(0, 0));
     }
     
-    public CustomJPanel(int[] border) {
+	public CustomJPanel(FlowLayout layout) {
     	super();
     	
     	this.setBackground(new Color(44, 47, 51));
-    	this.setBorder(new EmptyBorder(border[0], border[1], border[2], border[3]));
+    	this.setBorder(new EmptyBorder(0, 0, 0, 0));
+    	this.setLayout(layout);
+	}
+    
+    public CustomJPanel(EmptyBorder border) {
+    	super();
+    	
+    	this.setBackground(new Color(44, 47, 51));
     	this.setLayout(new BorderLayout(0, 0));
+    	this.setBorder(border);
+    }
+    
+    public CustomJPanel(EmptyBorder border, GridLayout layout) {
+    	super();
+    	
+    	this.setBackground(new Color(44, 47, 51));
+    	this.setBorder(border);
+    	this.setLayout(layout);
+    }
+    
+    public CustomJPanel(EmptyBorder border, FlowLayout layout) {
+    	super();
+    	
+    	this.setBackground(new Color(44, 47, 51));
+    	this.setBorder(border);
+    	this.setLayout(layout);
+    }
+    
+    public CustomJPanel(EmptyBorder border, GridBagLayout layout) {
+    	super();
+    	
+    	this.setBackground(new Color(44, 47, 51));
+    	this.setBorder(border);
+    	this.setLayout(layout);
     }
 }
