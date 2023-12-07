0736259efe09881b08479e6725cf9bef57e51710
Ryan <mordeckai.pro@gmail.com>
2023-12-07 16:43:53 +0100

    CustomJFrame : Ajout de plusieurs constructeur avec titre de fenêtre + Dimension

--- sae_esport/src/style/CustomJFrame.java
+++ sae_esport/src/style/CustomJFrame.java
@@ -3,18 +3,49 @@ package style;
 import java.awt.Dimension;
 
 import javax.swing.JFrame;
+import javax.swing.border.EmptyBorder;
 
 public class CustomJFrame extends JFrame {
 	CustomJPanel contentPanel;
 	
-	public CustomJFrame(int[] dimension) {
+	public CustomJFrame() {
 		super();
 		
-		this.contentPanel = new CustomJPanel(new int[]{30, 30, 30, 30});
-    	setMinimumSize(new Dimension(dimension[0], dimension[1]));
+		this.contentPanel = new CustomJPanel(new EmptyBorder(30, 30, 30, 30));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(100, 100, dimension[0], dimension[1]);
         setContentPane(this.contentPanel);
+        setResizable(false);
+	}
+	
+	public CustomJFrame(String frameTitle) {
+		super(frameTitle);
+		
+		this.contentPanel = new CustomJPanel(new EmptyBorder(30, 30, 30, 30));
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setContentPane(this.contentPanel);
+        setResizable(false);
+	}
+	
+	public CustomJFrame(Dimension dimension) {
+		super();
+		
+		this.contentPanel = new CustomJPanel(new EmptyBorder(30, 30, 30, 30));
+    	setMinimumSize(dimension);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, dimension.height, dimension.width);
+        setContentPane(this.contentPanel);
+        setResizable(false);
+	}
+	
+	public CustomJFrame(Dimension dimension, String frameTitle) {
+		super(frameTitle);
+		
+		this.contentPanel = new CustomJPanel(new EmptyBorder(30, 30, 30, 30));
+    	setMinimumSize(dimension);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, dimension.height, dimension.width);
+        setContentPane(this.contentPanel);
+        setResizable(false);
 	}
 	
 	public CustomJPanel getContentPanel( ) {
