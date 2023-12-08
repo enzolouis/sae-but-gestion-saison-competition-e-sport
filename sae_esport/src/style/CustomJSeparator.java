13134ad17997f5c57b0bda34715e4749db8bc058
Ryan <mordeckai.pro@gmail.com>
2023-12-08 13:47:23 +0100

    CustomJSeparator : Commentaires des définitions des constructeurs

--- sae_esport/src/style/CustomJSeparator.java
+++ sae_esport/src/style/CustomJSeparator.java
@@ -5,6 +5,12 @@ import java.awt.Color;
 import javax.swing.JSeparator;
 
 public class CustomJSeparator extends JSeparator {
+	
+	/**
+	* Ryan GAUNAND <br>
+	* Création d'un JSeparator avec l'application automatique de la charte graphique de l'application.
+	*
+	*/
 	public CustomJSeparator() {
 		super();
 		
