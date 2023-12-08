36985003ba3a40522bc0553cddcd8bb0f09c8785
Ryan <mordeckai.pro@gmail.com>
2023-12-08 13:46:54 +0100

    CustomJLabel : Commentaires des définitions des constructeurs

--- sae_esport/src/style/CustomJLabel.java
+++ sae_esport/src/style/CustomJLabel.java
@@ -8,6 +8,15 @@ import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 
 public class CustomJLabel extends JLabel {
+	
+	/**
+	* Ryan GAUNAND <br>
+	* Création d'un JLabel avec la définition d'un texte, la taille du texte et
+	* l'application automatique de la charte graphique de l'application.
+	*
+	* @param Texte principale
+	* @param Taille de la police du texte
+	*/
 	public CustomJLabel(String label, int fontScale) {
 		super(label);
 		
@@ -17,6 +26,15 @@ public class CustomJLabel extends JLabel {
         setBorder(new EmptyBorder(0, 0, 0, 0));
 	}
 	
+	/**
+	* Ryan GAUNAND <br>
+	* Création d'un JLabel avec la définition d'un texte, la taille du texte, de bordure vide et
+	* l'application automatique de la charte graphique de l'application.
+	*
+	* @param Texte principale
+	* @param Taille de la police du texte
+	* @param Bordure vide
+	*/
 	public CustomJLabel(String label, int fontScale, EmptyBorder border) {
 		super(label);
 		
