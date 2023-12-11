428925e54c7e020ab033e26ca58871ce039c0e75
Pema <rasoanaivo.pema@gmail.com>
2023-12-11 11:21:33 +0100

    EquipeDAO : correction du importEquipes

--- sae_esport/src/DAOs/EquipeDAO.java
+++ sae_esport/src/DAOs/EquipeDAO.java
@@ -12,6 +12,7 @@ import classes.DBConnection;
 import classes.Equipe;
 import classes.Joueur;
 import classes.Nationalite;
+import sqlrequests.CreateDual;
 
 public class EquipeDAO {
 	
@@ -41,7 +42,7 @@ public class EquipeDAO {
 			ResultSet res = reqSelectJoueursequipe.executeQuery();
 			List<Joueur> joueurs = new ArrayList<Joueur>();
 			while (res.next()) {
-				joueurs.add(new Joueur(rs.getInt(1),rs.getString(2),rs.getInt(3)));
+				joueurs.add(new Joueur(res.getInt(1),res.getString(2),res.getInt(3)));
 			}
 			equipes.add(new Equipe(rs.getInt(1),rs.getString(2),classes.Nationalite.valueOf(rs.getString(3)),joueurs,rs.getBoolean(4),rs.getInt(5),rs.getInt(6)));
 		}
@@ -52,12 +53,13 @@ public class EquipeDAO {
 	//peu importe l'id que vous mettrez à l'equipe, il sera changé
 	public boolean add(Equipe value) throws Exception {
 
-		PreparedStatement st = DBConnection.getInstance().prepareStatement("SELECT NEXT VALUE FOR seqIdEquipe FROM equipe");
+		PreparedStatement st = DBConnection.getInstance().prepareStatement("SELECT NEXT VALUE FOR seqIdEquipe FROM dual");
 		ResultSet rs = st.executeQuery();
 		int id = 0;
 		if (rs.next()) {
 			id = rs.getInt(1);
 		}
+		System.out.println(id);
 		value.setIdEquipe(id);
 		st = DBConnection.getInstance().prepareStatement("INSERT INTO equipe VALUES (?,?,?,?,?,?)");
 		st.setInt(1, id); 
@@ -72,19 +74,17 @@ public class EquipeDAO {
 			j.add(lj.get(i));
 		}
 		
-		int rowcount = st.executeUpdate();
-		return rowcount > 0;
+		return st.executeUpdate() > 0;
 		
 	}
 	
 	//ajoute des equipes d'un tableau css
 			public List<Equipe > importEquipes(File file) throws Exception{
+				
 				FileReader fr;
 				BufferedReader bfr;
-				
 				fr = new FileReader(file);
 				bfr= new BufferedReader(fr);
-				
 				
 				List<String[] > data = new ArrayList<String[] >();
 
@@ -95,55 +95,55 @@ public class EquipeDAO {
 				}
 				bfr.close();
 				
-				List<Equipe> le= getAll();
-				List<Equipe> et = new ArrayList<Equipe>();
+				List<Equipe> listEquipes = getAll();
+				List<Equipe> equipesTournoi = new ArrayList<Equipe>();
+				List<Joueur> listJoueurs = JoueurDAO.getInstance().getAll();
 				
 				for (String[] s : data) {
 					
-					Nationalite n = Nationalite.valueOf(s[1]);
-					JoueurDAO jdao = new JoueurDAO();
-					List<Joueur> lj = new ArrayList<Joueur>();
-					List<Joueur> j = JoueurDAO.getInstance().getAll();
+					Nationalite nat = Nationalite.valueOf(s[1]);
 					
-					PreparedStatement st = DBConnection.getInstance().prepareStatement("SELECT NEXT VALUE FOR seqIdEquipe FROM equipe");
-					ResultSet rs = st.executeQuery();
-					int id = 0;
-					if (rs.next()) {
-						id = rs.getInt(1);
-					}
+					List<Joueur> joueursEquipe = new ArrayList<Joueur>();
 					
-					Equipe e = new Equipe(id,s[0],n,lj,true,Integer.parseInt(s[2]),Integer.parseInt(s[3]));
-					et.add(e);
+					Equipe e = new Equipe(0,s[0],nat,joueursEquipe,true,Integer.parseInt(s[2]),Integer.parseInt(s[3]));
+					equipesTournoi.add(e);
+					int id = e.getIdEquipe();
 					
-					boolean t = true;
-					
-					for (Equipe eq : le) {
+					boolean nouvelleEquipe = true;
+					for (Equipe eq : listEquipes) {
 						if (e.getNom() == eq.getNom()) {
-							t = false;
+							nouvelleEquipe = false;
 						}
 					}
-					if (t) {
-						add(e);
-					}
+					
+					if (nouvelleEquipe) add(e);
 					
 					for (int i=4;i<=8;i++) {
-						Joueur jou = new Joueur(0,s[i],id);
-						boolean tj = true;
-						for (int k=0;k<j.size();k++) {
-							if (jou.getPseudo()==j.get(k).getPseudo()) {
-								tj = false;
+						
+						Joueur j = new Joueur(0,s[i],id);
+						boolean nouveauJoueur = true;
+						int idJ = 0;
+						for (Joueur jo : listJoueurs) {
+							if (jo.getPseudo() == j.getPseudo()) {
+								nouveauJoueur = false;
+								idJ = jo.getIdJoueur();
 							}
 						}
-						if (tj) {
-							jdao.add(jou);
-						}
 						
-						lj.add(jou);		
+						if (nouveauJoueur) {
+							JoueurDAO.getInstance().add(j);
+							joueursEquipe.add(j);
+						} else {
+							j.setIDJoueur(idJ);
+						}	
+						
+						e.AjouterJoueurs(j);
 						
 					}
 					
 				}
-				return et;
+				
+				return equipesTournoi;
 			}
 	
 	//update un equipe donné
