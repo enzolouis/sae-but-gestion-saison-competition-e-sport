692c1c3c713f4ba5bda3b87e75457f88d814ccc1
Pema <rasoanaivo.pema@gmail.com>
2023-12-11 12:02:26 +0100

    InsertValues : insérer les arbitres

--- sae_esport/src/sqlrequests/insertValues.java
+++ sae_esport/src/sqlrequests/insertValues.java
@@ -1,21 +1,24 @@
 package sqlrequests;
-import DAOs.AdministrateurDAO;
-import DAOs.TournoiDAO;
-import classes.Administrateur;
-import classes.EtatTournoi;
-import classes.Notoriete;
-import modeles.TournoiModele;
+
+import DAOs.ArbitreDAO;
+import classes.Arbitre;
+import classes.Nationalite;
 
 public class insertValues {
 
     public static void main(String[] args) throws Exception {
     	
-        //ajout d'administrateur
-        AdministrateurDAO adminDAO = new AdministrateurDAO();
-        adminDAO.add(new Administrateur(0, "admin1", "admin1", "motdepasse"));
-        System.out.print("Admin OK");
+    	//insertion des arbitres
+    	/*
+	    ArbitreDAO.getInstance().add(new Arbitre(0, "Dominique", "We", Nationalite.FR));
+	    ArbitreDAO.getInstance().add(new Arbitre(0, "Etoiles", "Rayou", Nationalite.DZ));
+	    ArbitreDAO.getInstance().add(new Arbitre(0, "Marc", "Evans", Nationalite.JP));
+	    ArbitreDAO.getInstance().add(new Arbitre(0, "Arbitre", "qui arbitre", Nationalite.BR));
+	    */
+    	ArbitreDAO.getInstance().add(new Arbitre(0, "Patrick", "Magnaud", Nationalite.IR));
+    	ArbitreDAO.getInstance().add(new Arbitre(0, "Chat", "Zelie", Nationalite.RU));
+    	ArbitreDAO.getInstance().add(new Arbitre(0, "Toxic", "Avenger", Nationalite.FR));
+    	ArbitreDAO.getInstance().add(new Arbitre(0, "Bonnard", "Rik", Nationalite.IS));
         
-        TournoiDAO.getInstance().add(new TournoiModele(0, "premierTournoi", "20/12/1888", "30/12/1888", Notoriete.LOCAL, EtatTournoi.OUVERT));
-        System.out.print("Tournoi OK");
     }
 }
