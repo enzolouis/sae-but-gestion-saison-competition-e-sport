706ead072bee046bc7d7a786989dd42e0e72ec50
Pema <rasoanaivo.pema@gmail.com>
2023-12-13 11:55:07 +0100

    TournoiDAO : maj pour relier à la table Participer et Gérer

--- sae_esport/src/DAOs/TournoiDAO.java
+++ sae_esport/src/DAOs/TournoiDAO.java
@@ -2,6 +2,7 @@ package DAOs;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -29,26 +30,33 @@ public class TournoiDAO {
 	
 	//Renvois l'ensemble des arbitres
 	public List<TournoiModele> getAll() throws Exception {
+		
 		ArrayList<TournoiModele> tournois = new ArrayList<>();
 		String reqSelectTournoi = "SELECT * FROM tournoi";
 		PreparedStatement st = DBConnection.getInstance().prepareStatement(reqSelectTournoi);
 		ResultSet rs = st.executeQuery();
-		String reqSelectParticipants = "SELECT idEquipe FROM Participer WHERE idTournoi = ?";
+		
+		String reqSelectParticipants = "SELECT idEquipe, resultat FROM Participer WHERE idTournoi = ?";
 		PreparedStatement stParticipants = DBConnection.getInstance().prepareStatement(reqSelectParticipants);
-		ArrayList<Equipe> participants = new ArrayList<>();
+		
+		PreparedStatement stArbitres = DBConnection.getInstance().prepareStatement("SELECT idArbitre FROM Gerer WHERE idTournoi = ?");
+		
 		while (rs.next()) {
 			SimpleDateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd");
 		    SimpleDateFormat outputFormat = new SimpleDateFormat("dd/MM/yyyy");
 			TournoiModele t = new TournoiModele(rs.getInt(1), rs.getString(2), outputFormat.format(inputFormat.parse(rs.getString(3))), outputFormat.format(inputFormat.parse(rs.getString(4))), rs.getString(5), rs.getString(6), classes.Notoriete.valueOf(rs.getString(7)), classes.EtatTournoi.valueOf(rs.getString(8)));
-			// TournoiModele t = new TournoiModele(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6), classes.Notoriete.valueOf(rs.getString(7)), classes.EtatTournoi.valueOf(rs.getString(8)));
 			stParticipants.setInt(1, rs.getInt(1));
 			ResultSet rsParticipants = stParticipants.executeQuery();
 			while (rsParticipants.next()) {
-				t.ajouterEquipe(null);
+				t.ajouterEquipe(EquipeDAO.getInstance().getById(rsParticipants.getInt(1)).get(),rsParticipants.getInt(2));
+			}
+			stArbitres.setInt(1, rs.getInt(1));
+			ResultSet rsArbitres = stArbitres.executeQuery();
+			while (rsArbitres.next()) {
+				t.ajouterArbitre(ArbitreDAO.getInstance().getById(rsArbitres.getInt(1)).get());
 			}
 			
 			tournois.add(t);
-			participants.clear();
 		}
 		return tournois;
 	}
@@ -63,13 +71,20 @@ public class TournoiDAO {
 			    SimpleDateFormat outputFormat = new SimpleDateFormat("dd/MM/yyyy");
 				TournoiModele t = new TournoiModele(rs.getInt(1), rs.getString(2), outputFormat.format(inputFormat.parse(rs.getString(3))), outputFormat.format(inputFormat.parse(rs.getString(4))), rs.getString(5), rs.getString(6), classes.Notoriete.valueOf(rs.getString(7)), classes.EtatTournoi.valueOf(rs.getString(8)));
 				
-				// TournoiModele t = new TournoiModele(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6), classes.Notoriete.valueOf(rs.getString(7)), classes.EtatTournoi.valueOf(rs.getString(8)));
-				PreparedStatement stParticipants = DBConnection.getInstance().prepareStatement("SELECT idEquipe FROM Participer WHERE idTournoi = ?");
+				PreparedStatement stParticipants = DBConnection.getInstance().prepareStatement("SELECT idEquipe, resultat FROM Participer WHERE idTournoi = ?");
 				stParticipants.setInt(1, rs.getInt(1));
 				ResultSet rsParticipants = stParticipants.executeQuery();
 				while (rsParticipants.next()) {
-					t.ajouterEquipe(null);
+					t.ajouterEquipe(EquipeDAO.getInstance().getById(rsParticipants.getInt(1)).get(),rsParticipants.getInt(2));
 				}
+				
+				PreparedStatement stArbitres = DBConnection.getInstance().prepareStatement("SELECT idArbitre FROM Gerer WHERE idTournoi = ?");
+				stArbitres.setInt(1, rs.getInt(1));
+				ResultSet rsArbitres = stArbitres.executeQuery();
+				while (rsArbitres.next()) {
+					t.ajouterArbitre(ArbitreDAO.getInstance().getById(rsArbitres.getInt(1)).get());
+				}
+				
 				return Optional.of(t);
 			}
 		}
@@ -140,6 +155,22 @@ public class TournoiDAO {
 		PreparedStatement stDelGerer = DBConnection.getInstance().prepareStatement("DELETE FROM gerer WHERE idTournoi=? AND idArbitre = ?");
 		stDelGerer.setInt(1, t.getIDTournoi()); stDelGerer.setInt(2, a.getIdArbitre());
 		int rowcount = stDelGerer.executeUpdate();
+		return rowcount > 0;
+		
+	}
+	
+	public boolean addEquipe (TournoiModele t, Equipe e) throws Exception {
+		PreparedStatement stAddParticiper = DBConnection.getInstance().prepareStatement("INSERT INTO participer VALUES(0,?,?)");
+		stAddParticiper.setInt(1,t.getIDTournoi()); stAddParticiper.setInt(2, e.getIdEquipe());
+		int rowcount = stAddParticiper.executeUpdate();
+		return rowcount > 0;
+	}
+	
+	public boolean deleteEquipe (TournoiModele t, Equipe e) throws Exception {
+		
+		PreparedStatement stDelEquipe = DBConnection.getInstance().prepareStatement("DELETE FROM participer WHERE idTournoi=? AND idEquipe = ?");
+		stDelEquipe.setInt(1, t.getIDTournoi()); stDelEquipe.setInt(2, e.getIdEquipe());
+		int rowcount = stDelEquipe.executeUpdate();
 		return rowcount > 0;
 		
 	}
