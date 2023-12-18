5af5a8af410d3dda1aecc938ebf7fb802a7cb4a9
Pema <rasoanaivo.pema@gmail.com>
2023-12-18 11:13:24 +0100

    ListeTournoisVue : finalisation

--- sae_esport/src/vues/ListeTournoisVue.java
+++ sae_esport/src/vues/ListeTournoisVue.java
@@ -5,6 +5,8 @@ import java.awt.EventQueue;
 
 import style.*;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -31,6 +33,9 @@ import javax.swing.ImageIcon;
 import javax.swing.JCheckBox;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
+import java.awt.Font;
+import java.awt.Rectangle;
+import javax.swing.JButton;
 
 public class ListeTournoisVue extends CustomJFrame {
 	
@@ -44,6 +49,7 @@ public class ListeTournoisVue extends CustomJFrame {
 	public CustomJTextField login;
 	public CustomJTextField mdp;
 	public JToggleButton activLogins;
+	public CustomJButton boutonOuverture;
 	
 	 public static void main(String[] args) {
 	        EventQueue.invokeLater(new Runnable() {
@@ -60,7 +66,7 @@ public class ListeTournoisVue extends CustomJFrame {
 	
 	public ListeTournoisVue() {
 		
-		setSize(new Dimension(600, 600));
+		setSize(new Dimension(600, 500));
 		setResizable(true);
 		
 		CustomJPanel panel = new CustomJPanel();
@@ -112,6 +118,7 @@ public class ListeTournoisVue extends CustomJFrame {
 		panelListe.add(scrollListe);
 		
 		CustomJPanel panelInfos = new CustomJPanel();
+		panelInfos.setBorder(new EmptyBorder(5, 5, 5, 5));
 		panelTournois.add(panelInfos);
 		panelInfos.setLayout(new GridLayout(0, 2, 0, 0));
 		
@@ -142,48 +149,79 @@ public class ListeTournoisVue extends CustomJFrame {
 		panelEquipeArbitre.add(panelArbitre);
 		
 		CustomJPanel titreTournoi = new CustomJPanel();
+		titreTournoi.setBorder(new EmptyBorder(5, 5, 5, 5));
 		CustomJPanel boutonsTournoi = new CustomJPanel();
 		CustomJPanel infosTournoi = new CustomJPanel();
 		panelAttributs.add(titreTournoi, BorderLayout.NORTH);
 		panelAttributs.add(boutonsTournoi, BorderLayout.SOUTH);
+		boutonsTournoi.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		panelAttributs.add(infosTournoi, BorderLayout.CENTER);
-		infosTournoi.setLayout(new GridLayout(2, 0, 0, 0));
 		
 		CustomJPanel logins = new CustomJPanel();
 		CustomJPanel dates = new CustomJPanel();
+		dates.setMaximumSize(new Dimension(2147483647, 15000));
+		infosTournoi.setLayout(new BoxLayout(infosTournoi, BoxLayout.Y_AXIS));
 		infosTournoi.add(logins); infosTournoi.add(dates);
 		
 		labelTitre = new CustomJLabel("Tournoi N°XX (Notoriété)", 15);
 		titreTournoi.add(labelTitre);
 		
 		dateDebut = new JDateChooser();
-		dateDebut.setBounds(59, 5, 70, 20);
+		dateDebut.setEnabled(false);
+		dateDebut.setBounds(30, 5, 90, 20);
 		dateFin = new JDateChooser();
-		dateFin.setBounds(134, 5, 70, 20);
+		dateFin.setEnabled(false);
+		dateFin.setBounds(152, 5, 90, 20);
 		dates.setLayout(null);
 		dates.add(dateDebut); dates.add(dateFin);
 		
 		login = new CustomJTextField();
-		login.setBounds(94, 20, 92, 20);
+		login.setBounds(94, 16, 92, 20);
 		mdp = new CustomJTextField();
-		mdp.setBounds(95, 51, 92, 20);
+		mdp.setBounds(94, 46, 92, 20);
 		CustomJLabel labelLogin = new CustomJLabel("Login d'arbitre", 11);
 		labelLogin.setHorizontalAlignment(SwingConstants.RIGHT);
 		labelLogin.setText("Identifiant");
-		labelLogin.setBounds(10, 15, 74, 30);
+		labelLogin.setBounds(10, 11, 74, 30);
 		CustomJLabel labelMdp = new CustomJLabel("Mot de passe d'arbitre", 11);
 		labelMdp.setHorizontalAlignment(SwingConstants.RIGHT);
 		labelMdp.setText("Mot de passe");
 		labelMdp.setSize(75, 30);
-		labelMdp.setLocation(10, 46);
+		labelMdp.setLocation(10, 41);
 		logins.add(labelLogin); logins.add(labelMdp);
 		
 		logins.setLayout(null);
 		logins.add(login); logins.add(mdp);
 		activLogins = new JToggleButton();
-		activLogins.setSelected(true);
-		activLogins.setBounds(203, 34, 33, 30);
+		activLogins.setFont(new Font("Tahoma", Font.PLAIN, 7));
+		activLogins.setText("Afficher");
+		activLogins.setBounds(193, 30, 65, 20);
 		logins.add(activLogins);
+		activLogins.addActionListener((new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (e.getSource() instanceof JToggleButton) {
+					JToggleButton bouton = (JToggleButton) e.getSource();
+					if (bouton.isSelected()) {
+						bouton.setText("Masquer");
+					} else {
+						bouton.setText("Afficher");
+					}
+				}
+			};
+		}
+		));
+		
+		CustomJLabel labelDateD = new CustomJLabel("Du", 15);
+		labelDateD.setFont(new Font("Tahoma", Font.BOLD, 11));
+		labelDateD.setBounds(0, 0, 32, 30);
+		CustomJLabel labelDateF = new CustomJLabel("au", 15);
+		labelDateF.setFont(new Font("Tahoma", Font.BOLD, 11));
+		labelDateF.setBounds(118, 0, 32, 30);
+		dates.add(labelDateD); dates.add(labelDateF);
+		boutonOuverture = new CustomJButton("Ouvrir", 15);
+		boutonsTournoi.add(boutonOuverture);
 		
 	}
+	
 }
