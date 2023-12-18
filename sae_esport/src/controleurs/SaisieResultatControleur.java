b7cac7bf6495983dc099f900aaad2dbadfcf1880
YatrezHD <146734983+YatrezHD@users.noreply.github.com>
2023-12-18 09:44:23 +0100

    SaisieResultatControleur: mise en place du getactionCommand et autres modifications pour mettre une équipe victorieuse d'un match avec traitement de la règle métier

--- sae_esport/src/controleurs/SaisieResultatControleur.java
+++ sae_esport/src/controleurs/SaisieResultatControleur.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.sql.SQLException;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
@@ -17,15 +18,16 @@ import classes.Match;
 import modeles.SaisieResultatModele;
 import modeles.TournoiModele;
 import vues.TournoiListeVue;
+import DAOs.MatchDAO;
 
 public class SaisieResultatControleur implements ActionListener {
 	private SaisieResultatModele modele;
 	private TournoiListeVue vue;
 	
 	
-	public SaisieResultatControleur(TournoiListeVue vue) {
+	public SaisieResultatControleur(TournoiListeVue vue, TournoiModele tournoi) {
 		this.vue = vue; 
-		this.modele = new SaisieResultatModele(new TournoiModele());
+		this.modele = new SaisieResultatModele(tournoi);
 	}
 
 	@Override
@@ -37,8 +39,19 @@ public class SaisieResultatControleur implements ActionListener {
 				this.vue.dispose();
 				break;
 			default:
-				//get match
-				//set vainqueur
+				String[] ids = bouton.getActionCommand().split(",");
+				int idmatch= Integer.valueOf(ids[0]);
+				int idequipe = Integer.valueOf(ids[1]);
+				Match m= new Match(idequipe,false);
+				try {
+					m = MatchDAO.getInstance().getById(idmatch).get();
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				if ( ! modele.IsFinaleDemarree() || m.IsItFinale()) {
+					m.setVainqueur(idequipe);
+				}
 			}	
 		}
 	}
