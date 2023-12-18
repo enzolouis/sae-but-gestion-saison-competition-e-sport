4ee62a8ee781b4bcf06dac3e0c6a3ace85e3ad27
YatrezHD <146734983+YatrezHD@users.noreply.github.com>
2023-12-18 11:28:12 +0100

    SaisieResultatControlleur: Traitement des boutons "Fermeture Tournoi" et "Demarrer Finale"

--- sae_esport/src/controleurs/SaisieResultatControleur.java
+++ sae_esport/src/controleurs/SaisieResultatControleur.java
@@ -6,7 +6,9 @@ import java.io.File;
 import java.sql.SQLException;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
@@ -14,7 +16,9 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
+import classes.Equipe;
 import classes.Match;
+import classes.EtatTournoi;
 import modeles.SaisieResultatModele;
 import modeles.TournoiModele;
 import vues.SaisieResultatVue;
@@ -38,11 +42,40 @@ public class SaisieResultatControleur implements ActionListener {
 			case ("Quitter"):
 				this.vue.dispose();
 				break;
+			case ("Ouvrir la Finale"):
+				Match ma = new Match(0,true);
+				Equipe e1;
+				Equipe e2;
+				int score= -1;
+				int score2 = -2;
+				Map<Equipe, Integer> equipe = modele.getTournoi().getEquipes();
+				for (Map.Entry eq : equipe.entrySet()) {
+					if((Integer) eq.getValue() > score) {
+						e2 = e1;
+						score2 = score;
+						e1 = (Equipe) eq.getKey();
+						score = (Integer)eq.getKey();
+					}
+					else {
+						if ((Integer) eq.getValue() > score2) {
+							e2 = (Equipe) eq.getKey();
+							score2 = (Integer)eq.getKey();
+						}
+					}
+				}
+				ma.AddEquipe(e1);
+				ma.AddEquipe(e2);
+				MatchDAO.getInstance().add(ma);
+				//Ajout au tournoi
+				break;
+			case ("Fermer le tournoi"):
+				modele.getTournoi().setEtatTournoi(EtatTournoi.FERME);				
+				break;
 			default:
 				String[] ids = bouton.getActionCommand().split(",");
 				int idmatch= Integer.valueOf(ids[0]);
 				int idequipe = Integer.valueOf(ids[1]);
-				Match m= new Match(idequipe,false);
+				Match m= new Match(idmatch,false);
 				try {
 					m = MatchDAO.getInstance().getById(idmatch).get();
 				} catch (Exception e1) {
