a7e830d73e8d2070e62050f02fe0d70dfe84752e
Pema <rasoanaivo.pema@gmail.com>
2024-01-09 10:08:45 +0100

    Tournoi : ajout de la récupération des matchs dans le DAO

--- sae_esport/src/modeles/TestSaisieResultatModele.java
+++ sae_esport/src/modeles/TestSaisieResultatModele.java
@@ -9,7 +9,7 @@ public class TestSaisieResultatModele {
 	
 	public static void main(String[] args) {
 		TournoiModele t = new TournoiModele();
-		Match ma = new Match(0, false);
+		Match ma = new Match(0, t.getIDTournoi(), false);
 		m = new SaisieResultatModele(t);
 		if (m.IsFinaleDemarree()) {
 			System.out.print("IsFinaleDemarree - tournoi vide erreur");
