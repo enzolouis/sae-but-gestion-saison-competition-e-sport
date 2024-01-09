d4b15453326fb84a7bd642b4a035f96623fc885d
YatrezHD <146734983+YatrezHD@users.noreply.github.com>
2024-01-09 12:04:44 +0100

    ClotureDatePassee: Creation (fichier permettant la fermeture automatique d'un tournoi si l'arbitre ne le fait pas)

---
+++ sae_esport/src/classes/ClotureDatePassee.java
@@ -0,0 +1,45 @@
+package classes;
+
+import java.util.Date;
+import java.util.Map;
+import java.util.TimerTask;
+
+import DAOs.EquipeDAO;
+import controleurs.SaisieResultatControleur;
+
+public class ClotureDatePassee extends TimerTask {
+	
+	SaisieResultatControleur c;
+	
+	public ClotureDatePassee (SaisieResultatControleur c) {
+		this.c = c;
+	}
+	
+	
+
+	  @Override
+	  public void run() {
+		  c.getModele().getTournoi().setEtatTournoi(EtatTournoi.FERME);
+			if (c.getModele().IsFinaleDemarree()) {
+				Map<Equipe, Integer> equipe = c.getModele().getTournoi().getEquipes();
+				for (Map.Entry eq : equipe.entrySet()) {
+					Equipe equi =(Equipe) eq.getKey();
+					Equipe equip = null;
+					try {
+						equip = EquipeDAO.getInstance().getById(equi.getIdEquipe()).get();
+					} catch (Exception e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					equip.setPointsSaison((Integer)eq.getValue() + equip.getPointsSaison());
+					try {
+						EquipeDAO.getInstance().update(equip);
+					} catch (Exception e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
+			}
+	  }
+
+}
