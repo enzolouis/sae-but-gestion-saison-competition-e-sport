96efda99634e8a8c3878b34aa612bbd29f916e8f
Pema <rasoanaivo.pema@gmail.com>
2024-01-10 11:59:02 +0100

    TestParticiperDAO : moved and renamed

--- sae_esport/src/tests/TestParticiperDAO.java
+++ sae_esport/src/tests/TestParticiperDAO.java
@@ -1,4 +1,4 @@
-package DAOs;
+package tests;
 
 import static org.junit.Assert.*;
 
@@ -12,17 +12,28 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import DAOs.EquipeDAO;
+import DAOs.ParticiperDAO;
 import classes.DBConnection;
+import classes.Disposition;
+import classes.Equipe;
 import classes.EtatTournoi;
+import classes.Nationalite;
 import classes.Notoriete;
 import classes.Participer;
 import modeles.TournoiModele;
 
-public class testParticiperDAO {
+public class TestParticiperDAO {
+	
+	private Equipe equipeTest;
+	private Equipe equipeTest2;
 	
 	@Before
 	public void beforeTests() throws Exception{
 			DBConnection.getInstance().setAutoCommit(false);
+			equipeTest = new Equipe(0, "nom", Nationalite.FR, Disposition.DISPOSEE, 0, 0);
+			equipeTest2 = new Equipe(0, "nom2", Nationalite.FR, Disposition.DISPOSEE, 0, 0);
+			EquipeDAO.getInstance().add(equipeTest); EquipeDAO.getInstance().add(equipeTest2);
 	}
 	
 	@After
@@ -47,7 +58,7 @@ public class testParticiperDAO {
 	@Test
 	/// Test de la récupération de participations par tournoi
 	public void testGetByIdTournoi() throws Exception{
-	    Participer participer = new Participer(1,1,15);
+	    Participer participer = new Participer(15,1,equipeTest.getIdEquipe());
 		ParticiperDAO.getInstance().add(participer);
 		List<Participer> participations = ParticiperDAO.getInstance().getByIdTournoi(participer.getIdTournoi());
 		assertTrue(participations.contains(participer));
@@ -56,19 +67,18 @@ public class testParticiperDAO {
 	
 	@Test
 	public void testGetByIdEquipe() throws Exception{
-	    Participer participer = new Participer(15,1,1);
+	    Participer participer = new Participer(15,1,equipeTest.getIdEquipe());
 		ParticiperDAO.getInstance().add(participer);
 		List<Participer> participations = ParticiperDAO.getInstance().getByIdEquipe(participer.getIdEquipe());
 		assertTrue(participations.contains(participer));
 		DBConnection.getInstance().rollback();
 	}
 	
-	
 	@Test
 	//Test de la récupération de toutes les participations
 	public void testGetAllParticiper() throws Exception{
-		    Participer participer1 = new Participer(15,1,1);
-			Participer participer2 = new Participer(15,2,1);
+		    Participer participer1 = new Participer(15,1,equipeTest.getIdEquipe());
+			Participer participer2 = new Participer(15,1,equipeTest2.getIdEquipe());
 			ParticiperDAO.getInstance().add(participer1);
 			ParticiperDAO.getInstance().add(participer2);
 			List<Participer> participations = ParticiperDAO.getInstance().getAll();
@@ -81,11 +91,10 @@ public class testParticiperDAO {
 	/// Test de l'ajout d'une participation
 	public void testAddViaTournoi() throws Exception {
 		
-		Participer participer = new Participer(15,1,1);
+		Participer participer = new Participer(15,1,equipeTest.getIdEquipe());
 		ParticiperDAO.getInstance().add(participer);
-	    assertEquals(participer, 
-	    		ParticiperDAO.getInstance().
-	    		getByIdTournoiIdEquipe(participer.getIdEquipe(), participer.getIdTournoi()));
+	    assertEquals(participer, ParticiperDAO.getInstance().
+	    		getByIdTournoiIdEquipe(participer.getIdEquipe(), participer.getIdTournoi()).get());
 	    DBConnection.getInstance().rollback();
 	    	
 	}
@@ -93,19 +102,19 @@ public class testParticiperDAO {
 	@Test
 	/// Test de la mise à jour d'une participation
 	public void testUpdate() throws Exception {
-		Participer participer = new Participer(15,1,1);
+		Participer participer = new Participer(15,1,equipeTest.getIdEquipe());
 	    ParticiperDAO.getInstance().add(participer);
 	    participer.setResultat(20);
 	    ParticiperDAO.getInstance().update(participer);
 	    assertEquals(participer,ParticiperDAO.getInstance()
-	    		.getByIdTournoiIdEquipe(participer.getIdEquipe(), participer.getIdTournoi()));
+	    		.getByIdTournoiIdEquipe(participer.getIdEquipe(), participer.getIdTournoi()).get());
 	    DBConnection.getInstance().rollback();
 	}
 	
 	@Test
 	/// Test de supression d'une participation
 	public void testDelete() throws Exception { 
-		Participer participer = new Participer(15,1,1);
+		Participer participer = new Participer(15,1,equipeTest.getIdEquipe());
 	    ParticiperDAO.getInstance().add(participer);
 	    int size = ParticiperDAO.getInstance().getAll().size();
 	    ParticiperDAO.getInstance().delete(participer);           
