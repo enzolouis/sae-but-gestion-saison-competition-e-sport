b30eb3abf14366ef4c9e6b7a6b5dd9b101013b84
Pema <rasoanaivo.pema@gmail.com>
2024-01-10 11:55:02 +0100

    AccueilArbitreControleur : refactoring

--- sae_esport/src/controleurs/AccueilArbitreControleur.java
+++ sae_esport/src/controleurs/AccueilArbitreControleur.java
@@ -7,9 +7,7 @@ import DAOs.TournoiDAO;
 import modeles.TournoiModele;
 import style.CustomJFrame;
 import vues.AccueilArbitreVue;
-import vues.ConsultationSaisonVue;
 import vues.ConsultationTournoiVue;
-import vues.CreationTournoiVue;
 
 public class AccueilArbitreControleur implements ActionListener {
 	
@@ -21,32 +19,37 @@ public class AccueilArbitreControleur implements ActionListener {
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		
 		if (e.getSource() instanceof JButton) {
+			
 			JButton bouton = (JButton) e.getSource();
 			CustomJFrame frame;
+			
 			switch (bouton.getText()) {
+			
 			case ("Quitter"):
 				this.vue.setVisible(false);
 				this.vue.dispose();
 				break;
+				
 			case ("Accès au tournoi"):
 				break;
+			
 			case ("Statistiques du tournoi"):
 				TournoiModele t = null;
 				try {
 					t = TournoiDAO.getInstance().getTournoiOuvert().get();
 				} catch (Exception e1) {
-					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
 				
 				frame = new ConsultationTournoiVue(t);
 				frame.setVisible(true);
-			
-				
 				break;
+				
 			default:
 				break;
+				
 			}
 		}
 	}
