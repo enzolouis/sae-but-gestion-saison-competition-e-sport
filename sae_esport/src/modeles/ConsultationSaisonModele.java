f46f527c63f88600d1c7cb49bf2b4d14550ae03a
Pema <rasoanaivo.pema@gmail.com>
2024-01-11 13:20:19 +0100

    ConsultationSaisonModele : changement de la récupération du classement en Map

--- sae_esport/src/modeles/ConsultationSaisonModele.java
+++ sae_esport/src/modeles/ConsultationSaisonModele.java
@@ -1,6 +1,9 @@
 package modeles;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 import DAOs.EquipeDAO;
@@ -8,10 +11,11 @@ import classes.Equipe;
 
 public class ConsultationSaisonModele {
 	
-	public List<Equipe> classement() throws Exception {
+	public List<Equipe> classementRanked() throws Exception {
+		
 		return EquipeDAO.getInstance().getAll()
 			.stream()
-			.sorted((e1, e2) -> {if (e1.getPointsSaison() > e2.getPointsSaison()) {
+			.sorted((e1, e2) -> {if (e1.getPointsSaison() < e2.getPointsSaison()) {
 									return 1;
 								} else if (e1.getPointsSaison() == e2.getPointsSaison()) {
 									return 0;
@@ -19,6 +23,33 @@ public class ConsultationSaisonModele {
 									return -1;
 								}
 			}).collect(Collectors.toList());
+		
+	}
+	
+	public Map<Equipe,Integer> classementWithRank() throws Exception {
+		
+		HashMap<Equipe,Integer> classement = new HashMap<Equipe,Integer>();
+		
+		int rang = 1;
+		int nbPointsPerRank = this.classementRanked().get(0).getPointsSaison();
+		int nbTeamPerRank = 1;
+		
+		classement.put(this.classementRanked().get(0), 1);
+		
+		for (Equipe e : this.classementRanked().subList(1, this.classementRanked().size())) {
+			if (e.getPointsSaison() == nbPointsPerRank) {
+				nbTeamPerRank++;
+				classement.put(e, rang);
+			} else {
+				rang+=nbTeamPerRank;
+				nbPointsPerRank=e.getPointsSaison();
+				nbTeamPerRank=1;
+				classement.put(e, rang);
+			}
+		}
+		
+		return classement;
+		
 	}
 
 }
