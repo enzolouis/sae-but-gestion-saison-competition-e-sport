883a9a243559dce86dd18eae53c3ab32e2f2de68
Pema <rasoanaivo.pema@gmail.com>
2024-01-15 11:38:02 +0100

    AdminDAO : modif commentaires

--- sae_esport/src/DAOs/AdministrateurDAO.java
+++ sae_esport/src/DAOs/AdministrateurDAO.java
@@ -9,6 +9,8 @@ import classes.DBConnection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
+import classes.Administrateur;
+
 public class AdministrateurDAO {
 	
 	private static AdministrateurDAO instance;
@@ -23,34 +25,50 @@ public class AdministrateurDAO {
 		return instance;
 	}
 		
-	//Renvoie l'ensemble des administrateurs
-	public List<classes.Administrateur> getAll() throws Exception {
+	/**
+	 * renvoie l'ensemble des administrateurs
+	 * */
+	public List<Administrateur> getAll() throws Exception {
+		
 		String reqSelectAdministrateur = "SELECT * FROM admin";
 		PreparedStatement st = DBConnection.getInstance().prepareStatement(reqSelectAdministrateur);
 		ResultSet rs = st.executeQuery();
-		ArrayList<classes.Administrateur> administrateur = new ArrayList<classes.Administrateur>();
+		
+		ArrayList<Administrateur> administrateur = new ArrayList<Administrateur>();
 		while (rs.next()) {
-			administrateur.add(new classes.Administrateur(rs.getInt(1),rs.getString(2),rs.getString(3),rs.getString(4)));
+			administrateur.add(new Administrateur(
+					rs.getInt(1),rs.getString(2),rs.getString(3),rs.getString(4)));
 			
 		}
+	
 		return administrateur;
+		
 	}
 	
-	//retourne un Administrateur specifique
-	public Optional<classes.Administrateur> getById(Integer... id) throws Exception {
+	/**
+	 * renvoie l'administrateur du premier id reconnu
+	 * @param identifiant(s) de l'admin
+	 * */
+	public Optional<Administrateur> getById(Integer... id) throws Exception {
+		
 		PreparedStatement st = DBConnection.getInstance().prepareStatement("SELECT * FROM admin WHERE idAdmin=?");
+		
 		for (Integer i : id) {
 			st.setInt(1, i);
 			ResultSet rs = st.executeQuery();
 			if (rs.next()) {
 				return Optional.of(new classes.Administrateur(i,rs.getString(2),rs.getString(3),rs.getString(4)));
-			}
+			}	
 		}
+		
 		return Optional.empty();
 	}
 	
-	//ajoute un administrateur à la liste
-	public boolean add(classes.Administrateur value) throws Exception {
+	/**
+	 * ajoute un administrateur à la liste
+	 * @param admin à ajouter
+	 * */
+	public boolean add(Administrateur value) throws Exception {
 		
 		PreparedStatement st = DBConnection.getInstance().prepareStatement("SELECT NEXT VALUE FOR seqIdAdmin FROM admin");
 		ResultSet rs = st.executeQuery();
@@ -70,8 +88,11 @@ public class AdministrateurDAO {
 			
 	}
 	
-	//update un administrateur donné
-	public boolean update(classes.Administrateur value) throws Exception {
+	/**
+	 * met à jour dans la bdd l'admin donné
+	 * @param admin à mettre à jour
+	 * */
+	public boolean update(Administrateur value) throws Exception {
 		
 		PreparedStatement st = DBConnection.getInstance().prepareStatement("UPDATE admin SET nom=?, login=?, motDePasse=? WHERE idAdmin=?");
 		st.setString(1, value.getNom()); st.setString(2, value.getLogin());
@@ -82,8 +103,10 @@ public class AdministrateurDAO {
 		
 	}
 	
-	//retire un administrateur donné
-	public boolean delete(classes.Administrateur value) throws Exception {
+	/**
+	 * supprime de la bdd un administrateur donné
+	 * */
+	public boolean delete(Administrateur value) throws Exception {
 		
 		PreparedStatement st = DBConnection.getInstance().prepareStatement("DELETE FROM admin WHERE idAdmin=?");
 		st.setInt(1, value.getIdAdministrateur());
